56413731ae5af583b4162712d6df241c8a2b64e2
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-25 17:52:33 +0300

    added Example 011

---
+++ Example011_Array/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] coolection)
+{
+    int length = coolection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        coolection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int len = col.Length;
+    int position = 0;
+    while (position < len)
+    {
+        Console.Write(Convert.ToString(col[position]) + ", ");
+        position++;
+    }
+}
+
+int IndexOf(int[] colection, int Find)
+{
+    int len = colection.Length;
+    int index = 0;
+    int position = -1;
+    while (len > index)
+    {
+        if (Find == colection[index])
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return(position);
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("");
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
