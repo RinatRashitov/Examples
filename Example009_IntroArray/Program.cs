e7d466fad5368540f4f58cffde3f0206490b820b
Ринат Рашитов <rinat_rashitov@mail.ru>
2022-09-25 15:37:44 +0300

    added example009

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array1 = { 11, 21, 31, 411, 15, 61, 17, 18, 19 };
+array1[0]=1201;
+
+int count =1;
+int max = array1[0];
+while(count<9)
+{
+    if(max<array1[count]) max=array1[count];
+    count = count +1;
+}
+Console.WriteLine(max);
